f393b913f7909e78b7d24e9d4847c122a420a4da
ne1410s <pgj646@gmail.com>
2022-06-27 19:37:29 +0100

    Add unit tests

--- source/ExtractorFunc/ExtractorFunc.UnitTests/Repos/DataExtractRepoTests.cs
+++ source/ExtractorFunc/ExtractorFunc.UnitTests/Repos/DataExtractRepoTests.cs
@@ -1,4 +1,8 @@
 ﻿using ExtractorFunc.Repos;
+using ExtractorFunc.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Moq;
 
 namespace ExtractorFunc.UnitTests.Repos;
 
@@ -10,4 +14,18 @@ public class DataExtractRepoTests
     [Fact]
     public void Test1()
     { }
+
+    private static DataExtractRepo GetSut(out Mock<IBlobClientService> mockService)
+    {
+        mockService = new Mock<IBlobClientService>();
+        var mockEnv = new Mock<IHostingEnvironment>();
+        mockEnv.Setup(m => m.IsDevelopment()).Returns(true);
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "ExportBlobContainerName", "export" },
+            }).Build();
+
+        return new DataExtractRepo(mockEnv.Object, config, mockService.Object);
+    }
 }
