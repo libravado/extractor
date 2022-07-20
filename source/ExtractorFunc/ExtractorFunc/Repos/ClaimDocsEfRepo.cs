9bf72a1a10b1b8639c8f72e88968effa357f9312
ne1410s <pgj646@gmail.com>
2022-07-20 20:54:37 +0100

    hardcode uris :-S

--- source/ExtractorFunc/ExtractorFunc/Repos/ClaimDocsEfRepo.cs
+++ source/ExtractorFunc/ExtractorFunc/Repos/ClaimDocsEfRepo.cs
@@ -20,18 +20,27 @@ public class ClaimDocsEfRepo : IClaimDocsRepo
         this.sourceDb = sourceDb;
     }
 
-    /// <inheritdoc/>
     public async Task<List<ClaimDocument>> GetClaimDocumentsAync(RunConfig runConfig)
-        => await sourceDb.ClaimUploads
-            .Include(cu => cu.Claim!.PracticePolicy)
-            .Where(cu => runConfig.PracticeIds == null || runConfig.PracticeIds.Contains(cu.Claim!.PracticePolicy.PracticeId))
-            .Where(cu => cu.Claim!.CreatedAt >= runConfig.ClaimsCreatedFrom && cu.Claim!.CreatedAt < runConfig.ClaimsCreatedTo)
-            .Where(cu => ClaimTypes.Contains(cu.Claim!.TypeId))
-            .Select(cu => new ClaimDocument(
-                cu.Claim!.PracticePolicy.PracticeId,
-                cu.Claim.Id,
-                (ClaimType)cu.Claim.TypeId,
-                (DocumentType)cu.TypeId,
-                cu.BlobUri))
-            .ToListAsync();
+    {
+        return new List<ClaimDocument>
+        {
+            new ClaimDocument(1, 1001, ClaimType.Claim, DocumentType.Invoice, "https://devstgsharedweu.blob.core.windows.net/source-docs/word-doc.docx"),
+            new ClaimDocument(1, 1002, ClaimType.Continuation, DocumentType.Note, "https://devstgsharedweu.blob.core.windows.net/source-docs/1.pdf"),
+        };
+    }
+
+    ///// <inheritdoc/>
+    //public async Task<List<ClaimDocument>> GetClaimDocumentsAync(RunConfig runConfig)
+    //    => await sourceDb.ClaimUploads
+    //        .Include(cu => cu.Claim!.PracticePolicy)
+    //        .Where(cu => runConfig.PracticeIds == null || runConfig.PracticeIds.Contains(cu.Claim!.PracticePolicy.PracticeId))
+    //        .Where(cu => cu.Claim!.CreatedAt >= runConfig.ClaimsCreatedFrom && cu.Claim!.CreatedAt < runConfig.ClaimsCreatedTo)
+    //        .Where(cu => ClaimTypes.Contains(cu.Claim!.TypeId))
+    //        .Select(cu => new ClaimDocument(
+    //            cu.Claim!.PracticePolicy.PracticeId,
+    //            cu.Claim.Id,
+    //            (ClaimType)cu.Claim.TypeId,
+    //            (DocumentType)cu.TypeId,
+    //            cu.BlobUri))
+    //        .ToListAsync();
 }
